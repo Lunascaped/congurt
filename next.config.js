77ce3408b59125af5e69e8fcdd1957593df16730
Lunascaped <21148758+Lunascaped@users.noreply.github.com>
2026-06-27 14:35:53 -0500

    headers

--- next.config.js
+++ next.config.js
@@ -31,6 +31,32 @@ const nextConfig = {
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+      {
+        source: "/static/:all*",
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
